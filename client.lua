d4ad1d6d647ba17d93907893ed30cc6879f6089a
beahm.io <20533571+beahmio@users.noreply.github.com>
2019-05-06 13:34:05 -0700

    complete overhaul
    read code comments for more info

--- client.lua
+++ client.lua
@@ -2,45 +2,62 @@
 --- Interior Lights, Made by FAXES ---
 --------------------------------------
 
---- Config ---
+-- Config --
 
-activateWay = 0 -- 0 = use command, 1 = use keybind, 2 = use both command and keybind
-keybindKey = 182 -- http://docs.fivem.net/game-references/controls/
-intCommand = "il" -- If using the command, what do you want the command to be?
+local activateWay = 2 -- 0 = use command, 1 = use keybind, 2 = use both command and keybind
+local keybindKey = 182 -- http://docs.fivem.net/game-references/controls/
+local intCommand = "il" -- If using the command, what do you want the command to be?
+local errorNotify = true -- enable/disable error notifications (not in a vehicle & attempting to change interior lighting)
+local errorTitle = true -- enable/disable title ontop of error notification (title is: Interior Lights)
 
+-- Light Toggling Function --
 
-
---- Code Shit ---
-
-function toggleInteriorLights(ped, veh)
-    if IsPedInVehicle(ped, veh, false) then
-        if IsVehicleInteriorLightOn(veh) then
-            SetVehicleInteriorlight(veh, false)
+function toggleInteriorLights()
+	local ped = GetPlayerPed(PlayerId()) -- get local player's ID
+	local veh = GetVehiclePedIsUsing(ped) -- get vehicle ID that local player is in
+    if veh ~= 0 then -- if GetVehiclePedIsUsing returned a 0, then they aren't in a vehicle
+        if IsVehicleInteriorLightOn(veh) then -- if light is on
+            SetVehicleInteriorlight(veh, false) -- turn interior light off
         else
-            SetVehicleInteriorlight(veh, true)
+            SetVehicleInteriorlight(veh, true) -- turn interior light on
         end
-    else
-        TriggerEvent("chatMessage", "^1^*You are not in a vehicle.")
+    elseif errorNotify then -- don't show error if errors are disabled
+        ShowNotification('You are not in a vehicle.')
     end
 end
 
-Citizen.CreateThread(function()
-	while true do
-        Citizen.Wait(0)
-        local ped = GetPlayerPed(PlayerId())
-        local veh = GetVehiclePedIsUsing(ped)
-        if activateWay == 1 or activateWay == 2 then
-            if IsControlJustPressed(1, keybindKey) then
-                toggleInteriorLights(ped, veh)
-            end
-        end
-	end
-end)
+-- Error Notification --
 
-RegisterCommand(intCommand, function(source, args, raw)
-    local ped = GetPlayerPed(PlayerId())
-    local veh = GetVehiclePedIsUsing(ped)
-    if activateWay == 0 or activateWay == 2 then
-        toggleInteriorLights(ped, veh)
-    end
-end)
+if errorNotify then -- check config... don't create this function, if it won't be used
+	function ShowNotification(text)
+		if errorTitle then -- if error titles are enabled
+			text = ('~y~~h~Interior Lights~h~~s~~n~' .. text) -- add title to error notification
+		end
+		SetNotificationTextEntry('STRING') -- set notification type
+		AddTextComponentString(text) -- add string to the notification we are going to display
+		local notification = DrawNotification(false, false) -- display the error & save as "notification" so we can remove it
+		Citizen.Wait(1000) -- how long to wait before removing the error notification
+		RemoveNotification(notification) -- remove error notification
+	end
+end
+
+-- Keypress Listening --
+
+if activateWay == 1 or activateWay == 2 then -- check config... don't create this infinite loop, if it won't be used
+	Citizen.CreateThread(function() -- creates a seperate thread
+		while true do -- infinite loop
+			Citizen.Wait(2) -- adjusted polling rate to further minimize resource usage
+			if IsControlJustPressed(1, keybindKey) then -- if control is pressed
+				toggleInteriorLights() -- run the Light Toggling Function
+			end
+		end
+	end)
+end
+
+-- Command Registering --
+
+if activateWay == 0 or activateWay == 2 then -- check config... don't register this command, if it won't be used
+	RegisterCommand(intCommand, function(source, args, raw) -- register command
+			toggleInteriorLights() -- run the Light Toggling Function
+	end)
+end
